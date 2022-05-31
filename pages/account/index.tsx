b2595fbc241ea6a6477d932dbd0e9e1480bcceca
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2022-05-31 15:13:03 -0700

    Scaffold account management

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
-import { Col, Container, Row } from 'react-bootstrap'
-import { Messages } from '../../components/Messages'
-import { SharedFiles } from '../../components/SharedFiles'
+import { Badge, Card, Col, Container, Form, InputGroup, Row, Table } from 'react-bootstrap'
+import DashboardCard from '../../components/Base/DashboardCard'
 import Sidebar from '../../components/Sidebar'
-import { VideoLessons } from '../../components/VideoLessons/'
+import Image from 'next/image';
 
 const Account: NextPage = () => {
   return (
@@ -13,22 +12,68 @@ const Account: NextPage = () => {
       <Container className="py-3 h-100 pe-4">
         <Row className="h-100">
           <Col>
-            <Messages messages={[
-              { id: '0', title: "Tuition Processed", dateSent: "Today", read: false },
-              { id: '1', title: "Tuition Processed", dateSent: "Today", read: true },
-              { id: '2', title: "Tuition Processed", dateSent: "Today", read: true },
-              { id: '3', title: "Tuition Processed", dateSent: "Today", read: true },
-            ]} />
-          </Col>
-          <Col className="d-flex flex-column gap-3">
-            <Row className="flex-fill shadow-sm overflow-scroll flex-shrink-0" style={{maxHeight: "50%"}}> 
-              <SharedFiles files={[
-                { fileId: 0, filename: "Mozart K545", dateShared: "2 mo. ago", downloadUrl: '#' },
-              ]} />
-            </Row>
-            <Row className="flex-fill shadow-sm">
-              <VideoLessons lessons={[]} />
-            </Row>
+            <DashboardCard className="p-3">
+              <h1>Profile</h1>
+              <div className="d-flex gap-5">
+                <Image
+                  className="rounded"
+                  width={150}
+                  height={150}
+                  src="https://ui-avatars.com/api/?name=Jane+Doe&size=300"
+                  layout="fixed"
+                  alt="Your profile picture"
+                />
+                <div className="flex-grow-1">
+                  <h2>Jane Doe</h2>
+                  <Form.Label>Phone</Form.Label>
+                  <Form.Control value="(000) 000 0000"></Form.Control>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control value="jane.doe@gmail.com"></Form.Control>
+                  <Form.Label>Password</Form.Label>
+                  <InputGroup>
+                    <Form.Control type="password" placeholder="password" value="password"></Form.Control>
+                    <InputGroup.Text><i className="bi-eye-fill opacity-75" /></InputGroup.Text>
+                  </InputGroup>
+                </div>
+              </div>
+              <h1>Enrollment</h1>
+              <div>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Student</th>
+                      <th>Teacher</th>
+                      <th>Tuition</th>
+                      <th>Schedule</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Jane Doe</td>
+                      <td>Johnny Appleseed</td>
+                      <td>$60 @ 1hr lesson</td>
+                      <td>Weekly</td>
+                      <td><Badge bg="success">Ongoing</Badge></td>
+                    </tr>
+                    <tr>
+                      <td>John Doe</td>
+                      <td>Marge Simpson</td>
+                      <td>$40 @ 30m lesson</td>
+                      <td>Weekly</td>
+                      <td><Badge bg="warning">Hold</Badge></td>
+                    </tr>
+                    <tr>
+                      <td>Lena Doe</td>
+                      <td>Marge Simpson</td>
+                      <td>$40 @ 30m lesson</td>
+                      <td>Bi-weekly</td>
+                      <td><Badge bg="danger">Discontinued</Badge></td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </DashboardCard>
           </Col>
         </Row>
       </Container>
